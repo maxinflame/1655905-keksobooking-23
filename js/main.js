7964356fb0a47e62aca60375e0d2458a583d6e4f
maxinflame <maxinflame@yandex.ru>
2021-06-28 20:56:04 +0300

    Исправление ошибок

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import {createAds} from './data.js';
 import {createCards} from './cards.js';
 
+const cardsListFragment = document.createDocumentFragment();
 const ads = createAds();
-const cards = createCards(ads);
+const cards = createCards(ads, cardsListFragment);
 const cardsList = document.querySelector('#map-canvas');
 cardsList.appendChild(cards.firstElementChild);
